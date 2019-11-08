c6d72d6b6aaf481a9f07edfa8c99cc933d24f994
Alex <57452894+EuronimusDead@users.noreply.github.com>
2019-11-08 14:30:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -6,69 +6,9 @@ var contractABI = [
 		"constant": false,
 		"inputs": [
 			{
-				"internalType": "string",
-				"name": "_name",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_homeAddress",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_id",
-				"type": "uint256"
-			}
-		],
-		"name": "chengePrivInfo",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_name",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_homeAddress",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_role",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address",
-				"name": "_user",
-				"type": "address"
-			},
-			{
-				"internalType": "string",
-				"name": "_postalIndex",
-				"type": "string"
-			}
-		],
-		"name": "createByAdmin",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "uint256",
+				"internalType": "address payable",
 				"name": "_recipientMoney",
-				"type": "uint256"
+				"type": "address"
 			},
 			{
 				"internalType": "uint256",
@@ -82,6 +22,41 @@ var contractABI = [
 			}
 		],
 		"name": "createTransfer",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_homeAddress",
+				"type": "string"
+			}
+		],
+		"name": "registration",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "cancelTransfer",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
@@ -97,6 +72,67 @@ var contractABI = [
 			}
 		],
 		"name": "deactivateUser",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "viewUser",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "getTransfer",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
@@ -131,22 +167,71 @@ var contractABI = [
 				"type": "string"
 			},
 			{
-				"internalType": "uint256",
+				"internalType": "string",
 				"name": "_homeAddress",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_role",
 				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			},
+			{
+				"internalType": "string",
+				"name": "_postalIndex",
+				"type": "string"
 			}
 		],
-		"name": "registration",
+		"name": "createByAdmin",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
-		"inputs": [],
+		"constant": false,
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_homeAddress",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_id",
+				"type": "uint256"
+			}
+		],
+		"name": "chengePrivInfo",
+		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
-		"type": "constructor"
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "viewMoney",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
 	},
 	{
 		"constant": true,
@@ -157,32 +242,27 @@ var contractABI = [
 				"type": "uint256"
 			}
 		],
-		"name": "viewUser",
+		"name": "viewTransfer",
 		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			},
 			{
 				"internalType": "address",
 				"name": "",
 				"type": "address"
 			},
 			{
-				"internalType": "string",
+				"internalType": "address payable",
 				"name": "",
-				"type": "string"
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
 			},
 			{
 				"internalType": "bool",
@@ -193,30 +273,55 @@ var contractABI = [
 		"payable": false,
 		"stateMutability": "view",
 		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
 	}
 ];
-var contractAddr = "0x12628A0fCe47A3F15210c1840B2F9e267545475F";
-//console.log(web3);
+var contractAddr = "0x4c1EaF37FceE92A2b1b14C020B729FE094AC6938";
 
 function login() {
     var log = document.getElementById("loginBox");
-    var pass = document.getElementById("pwdBox");
-    //alert(log.value + " " + pass.value);
-    address = log.value;
-    web3.eth.personal.unlockAccount(log.value, pass.value, 600).then(successAuth, failAuth);
+	var pass = document.getElementById("pwdBox");
+	if(pass.value === "" || log.value === "") {
+		alert("пароль и/или логин не введен");
+	} else {
+		web3.eth.personal.unlockAccount(log.value, pass.value, 600).then(successAuth, failAuth);
+		address = log.value;
+		web3.eth.defaultAccount = log.value;
+	}
 }
 
 function registrate() {
     var pwd1 = document.getElementById("pwdBoxRegFirst");
-    var pwd2 = document.getElementById("pwdBoxRegSecond");
-    if(pwd1.value === pwd2.value) {
+	var pwd2 = document.getElementById("pwdBoxRegSecond");
+	if(pwd1.value === "" || pwd2.value === ""){
+		alert("пароль не введен");
+	} else if(pwd1.value === pwd2.value) {
         web3.eth.personal.newAccount(pwd1.value).then(successReg, failAuth);
     } else {
         alert("Пароли не совпадают!");
     }
-    
-
 }
+async function showInfo() {
+	var userid = document.getElementById("idBox");
+	var userInfo;
+	if(userid.value !== "") {
+		userInfo = await contract.methods.viewUser(userid.value).call().catch(console.error);
+		if(userInfo !== undefined) {
+			document.getElementById("userInfoBox").style.display = "block";
+			document.getElementById("userName").innerHTML = userInfo[0];
+			document.getElementById("userHomeAddr").innerHTML = userInfo[1];
+		} else {
+			alert("Пользователь не найден!");
+		}
+	} else {
+		alert("id не введен");
+	}
+}	
 
 
 function showLogin() {
@@ -233,6 +338,7 @@ function showProfile() {
      document.getElementById("profileForm").style.display = "block";
    
 }
+
 
 function successReg(_result) {
     alert(_result);
